c0d4969c37cb8f90ff5853a7469040a82d8a9c8a
6nx68pf8th-glitch <6nx68pf8th@privaterelay.appleid.com>
2026-05-03 19:16:18 +0200

    Updated Styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
 import { useCart } from '../context/CartContext';
+import styles from './header.module.css';
+
 export const Header = () => {
   const { cartCount } = useCart();
 
   return (
-    <header>
-      <div>
+    <header className={styles.header}>
+      <div className={styles.logo}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100" height="100" fill="currentColor">
           <path d="M12 2L15.09 8.26L22 9L15.09 9.74L12 16L8.91 9.74L2 9L8.91 8.26L12 2Z"/>
         </svg>
       </div>
-      <div>
+      <div className={styles.cart}>
         <span >{cartCount}</span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
